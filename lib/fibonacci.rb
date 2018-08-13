e2d5d4681daa6a4bcea25bad605da45dce6ef773
Rizwan Akhtar <rakhtarr@spartaglobal.com>
2018-08-13 21:31:51 +0100

    did fibonacci sequence

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,4 +1,25 @@
 class Fibonacci
 
+  attr_accessor :fib_array
+  def initialize
+    @fib_array = []
+  end
 
-end 
+  def fib_loop(start_num, end_num)
+    (start_num..end_num).each do |i|
+      i + i
+      if i < 4000000
+        if i.even?
+          @fib_array.push i
+      end
+    end
+
+    sum = @fib_array.reduce 0 do |total,number |
+      total + number
+    end
+    puts @fib_array
+    end 
+  end
+  fib = Fibonacci.new
+  fib.fib_loop(0,4000000)
+end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,5 +1,15 @@
 require_relative 'spec_helper.rb'
+  describe Fibonacci do
+    before(:each) do
+      @times = Fibonacci.new
+    end
 
+    it "should find the sum of even value terms in fibonacci which values do not exceed 4 million" do
+    @times.fib_loop(0,4000000)
+    expect(@times).to respond_to(:fib_loop)
+    expect(@times.fib_array).to be_kind_of Array
+    expect(@times.fib_array).to eq 4613732
+  end
 
 
 
